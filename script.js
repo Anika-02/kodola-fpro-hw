b7677a30ddb7eb6158ba21f7ab2ca6ff5abf5cc0
anna.kodola <anya.kodola@gmail.com>
2023-08-19 15:50:49 +0300

    calculation code for homework 3.1

---
+++ script.js
@@ -0,0 +1,19 @@
+let firstNum = prompt('Input the first number: ', '2');
+let secondNum = prompt('Input the second number: ', '2');
+
+let num1 = Number(firstNum);
+let num2 = Number(secondNum);
+
+if(String(num1) === "NaN" || String(num2) === "NaN" || !String(firstNum).trim() || !String(secondNum).trim()){
+    alert("Wrong Input");
+}
+else if(num2 !== 0)
+{
+    alert(`${num1} + ${num2} = ` + (num1 + num2) + '\n'
+        + `${num1} - ${num2} = ` + (num1 - num2) + '\n'
+        + `${num1} * ${num2} = ` + (num1 * num2) + '\n'
+        + `${num1} / ${num2} = ` + (num1 / num2));
+}
+else {
+    alert('You can\'t divide by zero! Please enter another number');
+}
