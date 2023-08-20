1031357e60768e49fe53f1f4cd011f7848cb3aea
anna.kodola <anya.kodola@gmail.com>
2023-08-20 10:28:26 +0300

    updated script

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Код, який виводить повідомлення з урахуванням розділення повідомлення на 2 частини (більш логічно)
 let birthYear = prompt("Введіть рік народження");
 let age = 0;
 let currentYear = new Date();
@@ -54,3 +55,66 @@ switch(sport.toLowerCase()){
         break;
 }
 
+//Далі йде код, де усі повідомлення виводяться в одному алерті
+/*let birthYear = prompt("Введіть рік народження");
+let age = 0;
+let currentYear = new Date();
+currentYear = currentYear.getFullYear();
+
+if (!birthYear){
+    alert("Шкода, що Ви не захотіли ввести свій рік народження");
+    age = "-";
+}
+else {
+    while(birthYear > currentYear){
+        birthYear = Number(prompt("Введіть правильно рік народження"));
+    }
+    age = currentYear - Number(birthYear);
+}
+
+let city = prompt("В якому місті ви живете?");
+if (!city){
+    alert("Шкода, що Ви не захотіли ввести своє місто");
+    city = "-";
+}
+
+let sport = prompt("Який ваш улюблений вид спорту?");
+if (!sport){
+    alert("Шкода, що Ви не захотіли ввести свій улюблений вид спорту");
+    sport = "-";
+}
+
+let cityMessage = "";
+switch(city.toLowerCase()) {
+    case 'київ':
+        cityMessage = `Ти живеш у столиці України`;
+        break;
+    case 'лондон':
+        cityMessage = `Ти живеш у столиці Великобританії`;
+        break;
+    case 'вашингтон':
+        cityMessage = `Ти живеш у столиці США`;
+        break;
+    default:
+        cityMessage = `Ти живеш у місті ${city}`;
+        break;
+}
+
+let sportMessage = "";
+switch(sport.toLowerCase()){
+    case "футбол":
+        sportMessage = "Круто! Хочеш стати Ліонелем Мессі?";
+        break;
+    case "волейбол":
+        sportMessage = "Круто! Хочеш стати Джордан Ларсон?";
+        break;
+    case "плавання":
+        sportMessage = "Круто! Хочеш стати Раяном Лохте?";
+        break;
+    default:
+        sportMessage = "Круто!";
+        break;
+}
+
+const finalMessage = `Ваш вік ${age}. ${cityMessage}. ${sportMessage}`;
+alert(finalMessage);*/
