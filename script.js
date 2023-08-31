2dae13786738dbc638e3daa9f3339217318ac8fe
anna.kodola <anya.kodola@gmail.com>
2023-08-31 16:51:51 +0300

    tasks with user accounts

---
+++ script.js
@@ -0,0 +1,73 @@
+let users = [
+    {
+        "index": 0,
+        "isActive": true,
+        "balance": 2226.60,
+        "name": "Eugenia Sawyer",
+        "gender": "female",
+        "phone": "+1 (840) 583-3207",
+        "address": "949 John Street, Rose, Puerto Rico, 1857"
+    },
+    {
+        "index": 1,
+        "isActive": true,
+        "balance": 2613.77,
+        "name": "Pauline Gallegos",
+        "gender": "female",
+        "phone": "+1 (985) 593-3328",
+        "address": "328 Greenpoint Avenue, Torboy, North Dakota, 6857"
+    },
+    {
+        "index": 2,
+        "isActive": false,
+        "balance": 3976.41,
+        "name": "Middleton Chaney",
+        "gender": "male",
+        "phone": "+1 (995) 591-2478",
+        "address": "807 Fleet Walk, Brutus, Arkansas, 9783"
+    },
+    {
+        "index": 3,
+        "isActive": true,
+        "balance": 1934.58,
+        "name": "Burns Poole",
+        "gender": "male",
+        "phone": "+1 (885) 559-3422",
+        "address": "730 Seba Avenue, Osage, Alabama, 6290"
+    },
+    {
+        "index": 4,
+        "isActive": true,
+        "balance": 3261.65,
+        "name": "Mcfadden Horne",
+        "gender": "male",
+        "phone": "+1 (942) 565-3988",
+        "address": "120 Scholes Street, Kirk, Michigan, 1018"
+    },
+    {
+        "index": 5,
+        "isActive": false,
+        "balance": 1790.56,
+        "name": "Suzette Lewis",
+        "gender": "female",
+        "phone": "+1 (837) 586-3283",
+        "address": "314 Dunne Place, Bawcomville, Guam, 9053"
+    }
+]
+
+
+//Створення та ініціалізація масиву номерами користувачів, у яких баланс більше 2000
+let usersPhones = [];
+users.forEach(item => {
+    item.balance > 2000? usersPhones.push(item.phone): null;
+});
+
+console.log(usersPhones);
+
+//Розрахунок суми всіх балансів користувачів
+let sumOfBalances = 0;
+users.reduce((accumulator, item) => {
+    return sumOfBalances += +item.balance;
+}, 0);
+
+console.log("Сума всіх балансів користувачів: " + sumOfBalances.toFixed(2));
