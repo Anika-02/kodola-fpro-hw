a54dd379ee4cc609689428fc60acaf0d911ccc65
anna.kodola <anya.kodola@gmail.com>
2023-09-21 13:56:59 +0300

    updated

--- registrationScript.js
+++ registrationScript.js
@@ -5,7 +5,7 @@ function updateUserInfo(userData) {
     document.querySelector('#userSex').textContent = userData.sex;
     document.querySelector('#userCity').textContent = userData.city;
     document.querySelector('#userAddress').textContent = userData.address;
-    document.querySelector('#userLanguages').textContent = userData.language.join(' ');
+    document.querySelector('#userLanguages').textContent = userData.language.join(', ');
 }
 
 const userData = {
@@ -20,15 +20,21 @@ const userData = {
 
 const formElements = {
     name: document.querySelector('#name'),
+    nameError: document.querySelector('#nameError'),
     surname: document.querySelector('#surname'),
+    surnameError: document.querySelector('#surnameError'),
     birthdate: document.querySelector('#birthDate'),
+    birthError: document.querySelector('#birthError'),
     sex: document.querySelectorAll('input[name="sex"]'),
+    sexError: document.querySelector('#sexError'),
     city: document.querySelector('#city-select'),
+    cityError: document.querySelector('#cityError'),
     address: document.querySelector('#address'),
+    addressError: document.querySelector('#addressError'),
     languageCheckboxes: document.querySelectorAll('input[name="language"]'),
+    languageError: document.querySelector('#languageError'),
     saveButton: document.querySelector('#save-btn'),
     form: document.querySelector('#registration-form'),
-    errorMessage: document.querySelector('.error'),
     successMessage: document.querySelector('.success'),
     userInfoTable: document.querySelector('.info-table'),
     registrationForm: document.querySelector('.registration-form'),
@@ -79,21 +85,65 @@ formElements.form.addEventListener('submit', (e) => {
         top: 0,
         behavior: 'smooth',
     });
+
     let errorMarker = false;
 
-    for (let key in userData) {
-        if (userData[key] === '' || (Array.isArray(userData[key]) && userData[key].length === 0)) {
-            formElements.errorMessage.style.display = 'block';
-            formElements.errorMessage.textContent = `Please, input your ${key}`;
-            errorMarker = true;
-        }
+    if (userData.name === '') {
+        formElements.nameError.textContent = 'Please, input your name';
+        errorMarker = true;
+    } else {
+        formElements.nameError.textContent = '';
+    }
+
+    if (userData.surname === '') {
+        formElements.surnameError.textContent = 'Please, input your surname';
+        errorMarker = true;
+    } else {
+        formElements.surnameError.textContent = '';
+    }
+
+    if (userData.birthdate === '') {
+        formElements.birthError.textContent = 'Please, input your birthdate';
+        errorMarker = true;
+    } else {
+        formElements.birthError.textContent = '';
+    }
+
+    if (userData.sex === '') {
+        formElements.sexError.textContent = 'Please, select your sex';
+        errorMarker = true;
+    } else {
+        formElements.sexError.textContent = '';
+    }
+
+    if (userData.city === '') {
+        formElements.cityError.textContent = 'Please, select your city';
+        errorMarker = true;
+    } else {
+        formElements.cityError.textContent = '';
+    }
+
+    if (userData.address === '') {
+        formElements.addressError.textContent = 'Please, input your address';
+        errorMarker = true;
+    } else {
+        formElements.addressError.textContent = '';
+    }
+
+    if (userData.language.length === 0) {
+        formElements.languageError.textContent = 'Please, select at least one language';
+    } else {
+        formElements.languageError.textContent = '';
     }
 
     if (!errorMarker) {
-        formElements.errorMessage.style.display = 'none';
         formElements.successMessage.style.display = 'block';
         updateUserInfo(userData);
         formElements.userInfoTable.style.display = 'flex';
         formElements.registrationForm.style.display = 'none';
+    }
+    else {
+        formElements.userInfoTable.style.display = 'none';
+        formElements.registrationForm.style.display = 'flex';
     }
 });
